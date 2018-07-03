d965bd912ce71c23d97b62761040257034047741
BinarySoftware <mkmikolajek@icloud.com>
2018-07-03 13:09:39 +0200

    a

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('site/index');
+    res.render('index');
 });
 
 app.listen(port, function() {

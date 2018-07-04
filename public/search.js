206633ff599a2ffa2740bd47c2bb4df3eaed3369
BinarySoftware <mkmikolajek@icloud.com>
2018-07-04 13:59:15 +0200

    ready to deploy

---
+++ public/search.js
@@ -0,0 +1,19 @@
+function doSearch(text) {
+    if (window.find && window.getSelection) {
+        document.designMode = "on";
+        var sel = window.getSelection();
+        sel.collapse(document.body, 0);
+
+        while (window.find(text)) {
+            document.execCommand("HiliteColor", false, "yellow");
+            sel.collapseToEnd();
+        }
+        document.designMode = "off";
+    } else if (document.body.createTextRange) {
+        var textRange = document.body.createTextRange();
+        while (textRange.findText(text)) {
+            textRange.execCommand("BackColor", false, "yellow");
+            textRange.collapse(false);
+        }
+    }
+}

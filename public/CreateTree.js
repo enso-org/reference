a63d07c058ef09fabfa1ba14a267e59bb7dd0006
BinarySoftware <mkmikolajek@icloud.com>
2018-07-16 14:23:41 +0200

    major changes

--- public/CreateTree.js
+++ public/CreateTree.js
@@ -58,17 +58,40 @@ var obs = new MutationObserver(function(mutations, observer) {
             if (mutations[i].addedNodes[j].id == "treeMID") {
                 $('#treeMID').treed();
 
-                $('a[href^="#"]').on('click', function(event) {
-                    var target = $(this.getAttribute('href'));
-                    console.log(target);
-                    if (target.length) {
-                        event.preventDefault();
-                        $('html, body').stop().animate({
-                            scrollTop: target.offset().top
-                        }, 1000);
-                        console.log("c!");
-                    }
-                });
+                $('a[href*="#"]')
+                    // Remove links that don't actually link to anything
+                    .not('[href="#"]')
+                    .not('[href="#0"]')
+                    .click(function(event) {
+                        // On-page links
+                        if (
+                            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                            location.hostname == this.hostname
+                        ) {
+                            // Figure out element to scroll to
+                            var target = $(this.hash);
+                            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                            // Does a scroll target exist?
+                            if (target.length) {
+                                // Only prevent default if animation is actually gonna happen
+                                event.preventDefault();
+                                $('html, body').animate({
+                                    scrollTop: target.offset().top
+                                }, 1000, function() {
+                                    // Callback after animation
+                                    // Must change focus!
+                                    var $target = $(target);
+                                    $target.focus();
+                                    if ($target.is(":focus")) { // Checking if the target was focused
+                                        return false;
+                                    } else {
+                                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                        $target.focus(); // Set focus again
+                                    };
+                                });
+                            }
+                        }
+                    });
             }
         }
     }

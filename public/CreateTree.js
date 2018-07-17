f939c575f1088a290a3b3a2d0cbb68317fe8b462
BinarySoftware <mkmikolajek@icloud.com>
2018-07-17 11:34:31 +0200

    major changes

--- public/CreateTree.js
+++ public/CreateTree.js
@@ -57,40 +57,6 @@ var obs = new MutationObserver(function(mutations, observer) {
             // was a child added with ID of 'bar'?
             if (mutations[i].addedNodes[j].id == "treeMID") {
                 $('#treeMID').treed();
-
-                $('a[href*="#"]:not([href="#"]')
-                    .click(function(event) {
-                        // On-page links
-                        if (
-                            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                            location.hostname == this.hostname
-                        ) {
-                            // Figure out element to scroll to
-                            var target = $(this.hash);
-                            target = target.length ? target : $('[name=\"' + this.hash.slice(1) + '\"]');
-                            console.log(this.hash.slice(1))
-                                // Does a scroll target exist?
-                            console.log(target)
-                            if (target.length) {
-                                // Only prevent default if animation is actually gonna happen
-                                event.preventDefault();
-                                $('html, body').animate({
-                                    scrollTop: target.offset().top
-                                }, 1000, function() {
-                                    // Callback after animation
-                                    // Must change focus!
-                                    var $target = $(target);
-                                    $target.focus();
-                                    if ($target.is(":focus")) { // Checking if the target was focused
-                                        return false;
-                                    } else {
-                                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                                        $target.focus(); // Set focus again
-                                    };
-                                });
-                            }
-                        }
-                    });
             }
         }
     }

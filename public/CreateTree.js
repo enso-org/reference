fb21b789b6be9ae5cd1226d94e15c2ce6d79e100
BinarySoftware <mkmikolajek@icloud.com>
2018-07-18 09:50:52 +0200

    major changes

--- public/CreateTree.js
+++ public/CreateTree.js
@@ -70,7 +70,7 @@ var obs = new MutationObserver(function(mutations, observer) {
                             for (var i = 0; i < parents.length; i++) {
                                 console.log($(parents[i]))
                                 if ($(parents[i]).id != "menuPane") {
-                                    if ($(parents[i]).is("li")) {
+                                    if ($(parents[i]).is("li,a")) {
                                         $(parents[i]).addClass('active');
                                         if (i > 1) {
                                             $(parents[i]).children.addClass('active');

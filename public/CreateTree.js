7c493c0f443be2610ca5c2528e0daea4a18231ff
BinarySoftware <mkmikolajek@icloud.com>
2018-07-19 14:46:44 +0200

    Release v4.0, menu showing current position, openSource fonts, fixed style

--- public/CreateTree.js
+++ public/CreateTree.js
@@ -96,6 +96,25 @@ var obs = new MutationObserver(function(mutations, observer) {
                         }
                     });
                 });
+
+                // The function actually applying the offset
+                function offsetAnchor() {
+                    if (location.hash.length !== 0) {
+                        window.scrollTo(window.scrollX, window.scrollY - 50);
+                    }
+                }
+
+                // Captures click events of all <a> elements with href starting with #
+                $(document).on('click', 'a[href^="#"]', function(event) {
+                    // Click events are captured before hashchanges. Timeout
+                    // causes offsetAnchor to be called after the page jump.
+                    window.setTimeout(function() {
+                        offsetAnchor();
+                    }, 0);
+                });
+
+                // Set the offset when entering page with hash present in the url
+                window.setTimeout(offsetAnchor, 0);
             }
         }
     }

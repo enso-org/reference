c82c2e5ab93d8f210a59566024f4f0a442fbf85c
BinarySoftware <mkmikolajek@icloud.com>
2018-08-01 12:33:25 +0200

    Refactoring IP

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 8080
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+app.get("/", function(req, res) {
+  res.render("index")
+})
+
+app.listen(port, function() {})

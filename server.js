cedfe8f4b5bf19c65e6cbae7a07eb3d15bf684a1
BinarySoftware <mkmikolajek@icloud.com>
2018-08-01 22:17:35 +0200

    coffe Fixes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+require('coffeescript');
 var DepLinker = require('dep-linker');
 DepLinker.copyDependenciesTo('./public/Scripts');
 
@@ -8,6 +8,7 @@ var port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
     res.render("index")
 })

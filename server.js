343654b11fcac3d7a1763d23d709639eb6749413
BinarySoftware <mkmikolajek@icloud.com>
2018-08-01 21:25:23 +0200

    ch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 8080
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
+app.locals.lunaLogo = lunaLogo
 app.get("/", function(req, res) {
     res.render("index")
 })

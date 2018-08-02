fc4867a1c479dc1397b99db1859527162ee7b8ad
BinarySoftware <mkmikolajek@icloud.com>
2018-08-02 12:26:04 +0200

    remove spaghetti, fx less

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var lessMiddleware = require('less-middleware');
 
 var DepLinker = require('dep-linker');
 DepLinker.copyDependenciesTo('./public/Scripts');
@@ -9,7 +8,6 @@ var port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(lessMiddleware(__dirname + "/public/styles"));
 
 app.get("/", function(req, res) {
     res.render("index")

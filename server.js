dcab1c8087694ea0ab1a166232dab0d8364ea280
BinarySoftware <mkmikolajek@icloud.com>
2018-08-02 12:01:04 +0200

    express-compile

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require("express");
+import { Compiler } from 'express-compile';
 
 var DepLinker = require('dep-linker');
 DepLinker.copyDependenciesTo('./public/Scripts');
 
 var app = express();
 var port = process.env.PORT || 8080;
+app.use(Compiler({
+    root: '.',
+    cwd: 'public',
+    paths: ['public/**/*'],
+    ignore: ['node_modules/**/*'],
+    ignoreStyleCache: true
+}));
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
 
 app.get("/", function(req, res) {
     res.render("index")

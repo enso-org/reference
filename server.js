9385d9045c0e390aa8c3d88e2543e243928cbcee
BinarySoftware <mkmikolajek@icloud.com>
2018-08-02 11:53:48 +0200

    using lesscss

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
 
 app.get("/", function(req, res) {
     res.render("index")

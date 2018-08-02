3f99b8d3a67272300701180358d26bf350bfef95
BinarySoftware <mkmikolajek@icloud.com>
2018-08-02 15:56:36 +0200

    Tree menu refactorization

--- public/Scripts/TreeHelpers.js
+++ public/Scripts/TreeHelpers.js
@@ -1,24 +1,18 @@
+// Extension - create tree menu panel
 $.fn.extend({
-    treed: function(o) {
-
+    treed: function() {
         var openedClass = 'fa fa-caret-down';
         var closedClass = 'fa fa-caret-right';
 
-        if (typeof o != 'undefined') {
-            if (typeof o.openedClass != 'undefined') {
-                openedClass = o.openedClass;
-            }
-            if (typeof o.closedClass != 'undefined') {
-                closedClass = o.closedClass;
-            }
-        };
-
+        // create tree from selected element
         var tree = $(this);
         tree.addClass("treeMenu");
+        // create branches
         tree.find('li').has("ul").each(function() {
             var branch = $(this);
             branch.prepend("<i class='indicator " + closedClass + "'></i>");
             branch.addClass('branch');
+            //add click event
             branch.on('click', function(e) {
                 if (this == e.target) {
                     var icon = $(this).children('i:first');
@@ -29,27 +23,26 @@ $.fn.extend({
             branch.children().children().toggle();
         });
 
+        //add events for open/close indicators
         tree.find('.branch .indicator').each(function() {
             $(this).on('click', function() {
-                $(this).closest('li').click();
-            });
-        });
-
-        tree.find('.branch>button').each(function() {
-            $(this).on('click', function(e) {
                 $(this).closest('li').click();
             });
         });
     }
 });
 
-
+// setting up new observer
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var treeDataObserver = new MutationObserver(function(mutations, observer) {
+    // checking each mutation 
     for (var i = 0; i < mutations.length; ++i) {
         for (var j = 0; j < mutations[i].addedNodes.length; ++j) {
+            //check if browser rendered place for tree menu
             if (mutations[i].addedNodes[j].id == "treeMID") {
+                //creation of new tree menu
                 $('#treeMID').treed();
+                //setting up scrolling events
                 scrollableAfterDOMContentLoadedProperly();
 
                 function offsetAnchor() {
@@ -58,6 +51,7 @@ var treeDataObserver = new MutationObserver(function(mutations, observer) {
                     }
                 }
 
+                //activating proper places on click on menu
                 $(document).on('click', 'a[href^="#"]', function(event) {
                     window.setTimeout(function() {
                         offsetAnchor();
@@ -69,6 +63,7 @@ var treeDataObserver = new MutationObserver(function(mutations, observer) {
                 });
                 window.setTimeout(offsetAnchor, 0);
 
+                //going to proper anchor from url
                 if (window.location.hash != "") {
                     var url = encodeURI(window.location.hash);
                     location.href = url;
@@ -79,10 +74,12 @@ var treeDataObserver = new MutationObserver(function(mutations, observer) {
     }
 });
 
+// adding observer to mutate if only menu files were loaded properly
 treeDataObserver.observe($("#menuPane").get(0), {
     childList: true
 });
 
+// function for getting parents of each element as array
 var getParents = function(elem) {
     var parents = [];
     for (; elem && elem !== document; elem = elem.parentNode) {
@@ -92,17 +89,22 @@ var getParents = function(elem) {
 };
 
 var updateMenuActivity = function(url) {
+    //1. remove all active states on every scroll event
     $(".treeMenu li,a,ul,i").removeClass("active");
 
+    //loop through each element in menu
     $(".treeMenu a").each(function() {
+        //Check if current element is the one displayed
         if (url == (this.href)) {
             var parents = getParents(this);
 
+            //apply specific classes to proper parent elements of checked element
             for (var i = 0; i < parents.length; i++) {
                 if ($(parents[i]).attr("id") != "menuPane") {
                     if (i == 0) {
                         var activeItem = $(parents)[0];
 
+                        //checks if there is need to scroll menu
                         if (checkIfInView($(activeItem)) == false) {
                             var wH = $(window).height();
                             var activeY = $(activeItem).offset().top - scrollY;
@@ -114,11 +116,13 @@ var updateMenuActivity = function(url) {
                         }
                     }
 
+                    // adding active state for classess/functions
                     if ($(parents[i]).is("li") && i > 0) {
                         $(parents[i]).addClass('active');
                         $(parents[i]).children().addClass('active');
                     }
 
+                    // opening branches and changing triangular indicators
                     if (i > 1 && $(parents[i]).attr("class") != "branch") {
                         $(parents[i]).css("display", "");
                         var openedClass = 'fa fa-caret-down';
@@ -132,6 +136,7 @@ var updateMenuActivity = function(url) {
                         $(parents[i]).children().css("display", "");
                     }
 
+                    //changing state of proper module
                     if ($(parents[3]).children()[1] !== undefined) {
                         array = $(parents[3]).children()[1].href.split("_")
 
@@ -151,11 +156,14 @@ var updateMenuActivity = function(url) {
 };
 
 function checkIfInView(elem, partial) {
+    //get the window heights
     var container = $(".sidenav");
     var contHeight = container.height();
     var contTop = container.scrollTop();
     var contBottom = contTop + contHeight;
 
+    //check if highlighted element is on screen by getting its position and
+    //comparing against container sizes
     var elemTop = $(elem).offset().top - container.offset().top;
     var elemBottom = elemTop + $(elem).height();
 

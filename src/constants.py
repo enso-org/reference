8e030015d21590f261d6c0c7dee57b90b1aa96eb
BinarySoftware <mkmikolajek@icloud.com>
2021-02-19 21:03:43 +0100

    docs

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,14 @@
 File containing all the constants.
 """
 
+#: GitHub organization from which std-lib will be downloaded.
 ORGANIZATION: str = "enso-org"
+#: Repository of mentioned organization from which std-lib will be downloaded.
 REPO: str = "enso"
+#: Branch of mentioned repository from which std-lib will be downloaded.
 BRANCH: str = "main"
+#: Directory from mentioned branch from which std-lib will be downloaded.
 DIRECTORY: str = "distribution/std-lib"
+
+#: Commit from which scala parser will be obtained.
 PARSER_COMMIT: str = "5e309bddcbec33cfbd150fcb8a16b45192cf5189"

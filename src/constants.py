2961eb090c111fabe5a75299c2c25bd5591b590c
BinarySoftware <mkmikolajek@icloud.com>
2021-02-23 13:38:20 +0100

    proper docs

--- src/constants.py
+++ src/constants.py
@@ -2,26 +2,29 @@
 File containing all the constants.
 """
 
-#: GitHub organization from which std-lib will be downloaded.
+#: The GitHub organization from which the standard libraries will be downloaded.
 ORGANIZATION: str = "enso-org"
-#: Repository of mentioned organization from which std-lib will be downloaded.
+#: The repository in the above organization from which standard libraries
+#: will be downloaded.
 REPO: str = "enso"
-#: Branch of mentioned repository from which std-lib will be downloaded.
+#: The branch in the above repository from which standard libraries
+#: will be downloaded.
 BRANCH: str = "main"
-#: Directory from mentioned branch from which std-lib will be downloaded.
+#: The directory in the above branch from which standard libraries
+#: will be downloaded.
 DIRECTORY: str = "distribution/std-lib"
 
-#: Commit from which scala parser will be obtained.
+#: The commit from which scala parser will be obtained.
 PARSER_COMMIT: str = "5c735b0ae7fc14bf72f5c0bf43b17c3fdf5d86b4"
-#: Scala parser package URL.
+#: The URL used to download scala parser package.
 PARSER_URL: str = "https://packages.luna-lang.org/parser-js/nightly/"
-#: IDE repository URL.
+#: The URL leading to IDE repository.
 IDE_REPO_URL: str = "https://raw.githubusercontent.com/enso-org/ide/"
 
-#: Enso file extension.
+#: The Enso source file extension.
 FILE_EXT: str = ".enso"
 
-#: Input directory.
+#: The input directory, into which all downloads will go.
 IN_DIR: str = "distribution"
-#: Output directory.
+#: The output directory, into which all generated content will go.
 OUT_DIR: str = "gen"

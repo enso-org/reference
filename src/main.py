5b7292a295ffe2381460b7f88c7acd0040c2daa8
BinarySoftware <mkmikolajek@icloud.com>
2021-02-24 01:45:53 +0100

    better args

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 Enso standard library documentation generator.
 """
 import argparse
+import constants
 from downloaders import download_stdlib, download_parser, download_stylesheet
 from parse import init_parser, init_gen_dir, gen_all_files
 
@@ -24,5 +25,19 @@ if __name__ == "__main__":
         description="Generates documentation sites for Enso Standard Library."
     )
     arg_parser.add_argument("token", help="GitHub user Personal Access Token.")
+    arg_parser.add_argument(
+        "--org", default=constants.ORGANIZATION, help="GitHub organization."
+    )
+    arg_parser.add_argument(
+        "--repo", default=constants.REPO, help="Organization's repository."
+    )
+    arg_parser.add_argument("--br", default=constants.BRANCH, help="Repository branch.")
+    arg_parser.add_argument(
+        "--dir", default=constants.DIRECTORY, help="Repository directory."
+    )
     args = arg_parser.parse_args()
+    constants.ORGANIZATION = args.org
+    constants.REPO = args.repo
+    constants.BRANCH = args.br
+    constants.DIRECTORY = args.dir
     main(args.token)
